5f4e35c2f2557d9b6e63ab6062d061cc8e021f81
VictorTheHollow <tonwevictor@gmail.com>
2025-12-10 16:36:37 +0100

    Risoluzione parte 2

--- main.java
+++ main.java
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 // Commento che da errore
-=======
 //modifica Giulio
->>>>>>> 6f4288489231eca1646caa252f7a74d730d75be2
 public static void main(){
   System.out.println("Hello World");
 }
